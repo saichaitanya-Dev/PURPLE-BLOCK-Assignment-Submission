8c5c9590fc22d2372d6d05573d04542c1b93aded
Sai Chaitanya <saichaitanyanukanaboina@gmail.com>
2025-03-21 12:46:15 +0530

    Create Assessment.py

---
+++ Assessment.py
@@ -0,0 +1,19 @@
+def best_trade(prices):
+    if not prices or len(prices) < 2:
+        return None  # Not enough prices to make a trade
+
+    min_price = prices[0]
+    max_profit = 0
+    best_buy = best_sell = prices[0]
+    for price in prices:
+        if price < min_price:
+            min_price = price  # Update minimum price
+        if price - min_price > max_profit:
+            max_profit = price - min_price
+            best_buy = min_price
+            best_sell = price
+
+    return best_buy, best_sell
+
+prices = [5, 2, 1, 6, 9, 7]
+print(best_trade(prices))
